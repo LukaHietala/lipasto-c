3d96b42a3b5d8cd03e2db47f60c514aa6576e47c
Luka Hietala <luka.hietala08@gmail.com>
2025-12-11 19:01:33 +0200

    remove tmp file leftovers from air, add bare repo struct and and clang-format from git

--- internal/git/lipasto.h
+++ internal/git/lipasto.h
@@ -4,7 +4,21 @@
 /*
  * Size of regular SHA-1 hash in git + 1
  * GIT_OID_SHA_HEXSIZE + 1
-*/
+ */
 #define OID_MAX_SIZE 41
+
+struct bare_repository {
+	char *path;
+	char *name;
+	char *owner;
+	char *description;
+	/*
+	 * If hide is set to 1 it hides the repository from index. If ignore is
+	 * set to 1 it hides the repository from index AND prevents it being
+	 * accessed via direct path.
+	 */
+	int hide;
+	int ignore;
+};
 
 #endif /* LIPASTO_H */
